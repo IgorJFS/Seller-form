6c1a5d34423393f768e86237b194803bf151b207
Igor Moraes <igormpaz@gmail.com>
2025-02-13 03:54:32 -0300

    Add files via upload

---
+++ file.js
@@ -0,0 +1,14 @@
+document.getElementById("password").addEventListener("input", function(event) {
+    this.value = this.value.replace(/\D/g, ""); 
+});
+document.getElementById("togglePassword").addEventListener("click", function(){
+    const showPW = document.getElementById("password")
+    const icon = document.querySelector(".material-symbols-outlined");
+    if (showPW.type === "password") {
+        showPW.type = "text";
+        icon.textContent = "visibility"; 
+    } else {
+        showPW.type = "password";
+        icon.textContent = "visibility_off"; 
+    }
+})
